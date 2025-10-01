a0f6374929224c3cdae73e919554abd24b7d4334
Tuilatanlocnee <huynhtanloc432004@gmail.com>
2025-10-01 21:28:11 +0700

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,18 +6,8 @@ use App\Models\Phim;
 
 class HomeController extends Controller
 {
-    public function phuongThucXemPhim($id)
-    {
-        // 1. Tìm phim theo ID
-        // $phim = Phim::findOrFail($id); // Giả sử bạn có Model Phim
+   
 
-        // 2. Trả về view và truyền dữ liệu phim
-        // return view('ten_view_xem_phim', compact('phim'));
-
-        // Tạm thời, để kiểm tra xem đã hết lỗi chưa:
-        return "Đang xem phim có ID: " . $id;
-    }
-    
     public function index()
     {
         $phimMoi = Phim::where('hien_thi', 'moi')
--- routes/web.php
+++ routes/web.php
@@ -47,11 +47,7 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
-// routes/web.php
 
-// Định nghĩa route cho việc xem một bộ phim
-Route::get('/xem-phim/{phim}', [HomeController::class, 'phuongThucXemPhim'])
-      ->name('xemphim'); // Đặt tên route là 'xemphim'
 
 
 
@@ -87,5 +83,4 @@ Route::prefix('admin/phim')->name('phim.')->group(function () {
 
     // Lưu phim mới
     Route::post('/them', [PhimController::class, 'store'])->name('store');
-
 });
